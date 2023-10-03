49e37c254c9119a6418105aae1aeb86adfd2f5c4
ninjafiveo <mjsekol@live.com>
2023-10-03 08:17:52 -0400

    Added Save Feature

--- classes_example.py
+++ classes_example.py
@@ -7,6 +7,7 @@ class Dog:
         self.name = name
         self.age = age
         self.breed = breed
+        self.save_dog_info()  # Calls save_dog_info method when a new Dog object is created
 
     # Method
     def bark(self):
@@ -19,17 +20,23 @@ class Dog:
         answer = num1 + num2
         return f"{num1} + {num2} = snack time for {self.name} and {answer} for hoomans."
     
+    def save_dog_info(self):
+        with open("doggos.txt", "a") as file:
+            file.write(f"{self.name}, {self.age}, {self.species}, {self.breed}\n")
+    
     
 
 # Creating objects of the class Dog
 dog1 = Dog("Buddy", 8, "Golden Retriever")
 dog2 = Dog("Mozzy", 4, "Multipoo")
+dog3 = Dog("Ralph", 2, "Beagle")
 
 # Accessing attributes and calling methods
 print(dog1.name)     # Outputs: Buddy
 print(dog2.age)      # Outputs: 5
 print(dog1.bark())   # Outputs: Buddy says Woof!
 print(dog2.dog_math(3, 9)) # Ouputs: 3 + 9 = snack time for Mozzy and 12 for hoomans.
+print(dog3.dog_math(7, 35)) # Ouputs: 7 + 35 = snack time for Mozzy and 42 for hoomans.
 
 
 
